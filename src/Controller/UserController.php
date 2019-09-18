c941f198651ff79232cfd65c440b6810e21437f5
danykyroz <danykyroz@gmail.com>
2019-09-17 22:01:22 -0500

    new login personalizado

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,18 +8,116 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\Type\RegistroUsuariosFormType;
 
-use FOS\UserBundle\Model\UserInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class UserController extends Controller
 {
     
 	private $userManager;
-    
-	 public function __construct(UserManagerInterface $userManager)
+    private $factory;
+    private $tokenManager;
+    private $managerInterface;
+	
+    public function __construct(UserManagerInterface $userManager, EncoderFactoryInterface $factory,CsrfTokenManagerInterface $tokenManager = null, EntityManagerInterface $managerInterface )
     {
         $this->userManager = $userManager;
-                
+        $this->factory=$factory;    
+        $this->tokenManager = $tokenManager;  
+        $this->managerInterface= $managerInterface;
+    }
+    /**
+     * @Route("/user/chek_login", name="user_check_login")
+    */
+    public function check_login(Request $request)
+    {
+       
+        $user = $this->userManager->createUser();
+        $username=$request->get('_username'); 
+        $em=$this->getDoctrine()->getManager();
+        $password_correcto=false;
+
+        $fosuser = $this->managerInterface->getRepository("App:User")->findOneBy(["username" => $username]);
+
+        if(!$fosuser){
+
+            $fosuser = $this->managerInterface->getRepository("App:User")->findOneBy(["email" => $username]);
+
+        }else{
+            $password=$request->get('_password'); 
+            $encoder = $this->factory->getEncoder($fosuser);
+
+                    if ($encoder->isPasswordValid($fosuser->getPassword(),
+                        $password,
+                        $fosuser->getSalt())) {
+                        $status = true;
+                        $message = "Login satisfactorio";
+                        $this->loginUser($fosuser, $request);
+
+                            if ($request->getSession()->get('_security.main.target_path') != "") {
+                                $redirect = $request->getSession()->get('_security.main.target_path');
+                            } else {
+                                $redirect = '/admin/home';
+                            }
+                        return $this->redirect($redirect);
+
+                    } else {
+                        $message = "Contraseña invalida";
+                        $fosuser=false;
+                    }
+         
+
+
+        }
+
+        $session = $request->getSession();
+
+        $authErrorKey = Security::AUTHENTICATION_ERROR;
+        $lastUsernameKey = Security::LAST_USERNAME;
+
+        if ($request->attributes->has($authErrorKey)) {
+            $error = $request->attributes->get($authErrorKey);
+        } elseif (null !== $session && $session->has($authErrorKey)) {
+            $error = $session->get($authErrorKey);
+            $session->remove($authErrorKey);
+        } else {
+            $error = null;
+        }
+
+        if (!$error instanceof AuthenticationException) {
+            $error = null; // The value does not come from the security component.
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+
+         $csrfToken = $this->tokenManager
+            ? $this->tokenManager->getToken('authenticate')->getValue()
+            : null;
+
+        if(!$fosuser || !$password_correcto){
+
+             $this->addFlash('bad', 'usuario o contraseña invalida');
+
+            $data=array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token' => $csrfToken,
+            );
+            return $this->render('@FOSUser/Security/login.html.twig', $data);
+            //return $this->renderLogin();
+        }
+           
     }
 
 
@@ -77,6 +175,18 @@ class UserController extends Controller
 		}
 
        return $this->render('/user/new.html.twig',['form'=>$form->createView()]);
+    }
+
+    /**
+     * @param $user GrantUser
+     * @param $request
+     */
+    private function loginUser($user, $request)
+    {
+        $token = new UsernamePasswordToken($user, null, "main", $user->getRoles());
+        $this->get('security.token_storage')->setToken($token); //now the user is logged in
+        $event = new InteractiveLoginEvent($request, $token);
+        $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
     }
 
 
