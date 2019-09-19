af91532e183e0b3d55df4a6f044be9e382c4bc87
danykyroz <danykyroz@gmail.com>
2019-09-19 01:13:09 -0500

    punto de venta admin crud

--- src/Form/PuntoVentaType.php
+++ src/Form/PuntoVentaType.php
@@ -6,18 +6,18 @@ use App\Entity\PuntoVenta;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class PuntoVentaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('documento')
-            ->add('nombres')
-            ->add('apellidos')
-            ->add('email')
-            ->add('celular')
-            
+            ->add('documento',null,array('required'=>true))
+            ->add('nombres',null,array('required'=>true))
+            ->add('apellidos',null,array('required'=>true))
+            ->add('email', EmailType::class, array('label' => 'Email'))
+            ->add('celular',null,array('required'=>true))
         ;
     }
 
