65db9282804c84e68fbe2228b676d085045aff22
danykyroz <danykyroz@gmail.com>
2019-10-08 00:00:11 -0500

    secuenta app

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,13 +6,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Controller\UserController;
 
-class DefaultController extends Controller
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+/**
+ * @Route("/app")
+ */
+
+class AppController extends UserController
 {
+
+	private $userManager;
+    private $factory;
+    private $tokenManager;
+    private $managerInterface;
+	
+    public function __construct(UserManagerInterface $userManager, EncoderFactoryInterface $factory,CsrfTokenManagerInterface $tokenManager = null, EntityManagerInterface $managerInterface )
+    {
+        $this->userManager = $userManager;
+        $this->factory=$factory;    
+        $this->tokenManager = $tokenManager;  
+        $this->managerInterface= $managerInterface;
+    }
 
 
   /**
-   * @Route("/app", name="app_index")
+   * @Route("/", name="app_index")
    */
 
   public function index(Request $request)
@@ -20,18 +50,39 @@ class DefaultController extends Controller
   	 return $this->render('app/index.html.twig'); 
   }
 
+
   /**
-   * @Route("/app/splash", name="app_login")
+   * @Route("/slider", name="app_slider")
+  */
+
+  public function slider(Request $request)
+  {
+		return $this->render('app/slider.html.twig'); 
+  }
+
+  /**
+   * @Route("/login", name="app_login")
    */
 
   public function login(Request $request)
   {
-  	 return $this->render('landing/login.html.twig'); 
+  	  $csrfToken = $this->tokenManager
+            ? $this->tokenManager->getToken('authenticate')->getValue()
+            : null;
+
+
+         $data=array(
+            'last_username' => '',
+            'error' => '',
+            'csrf_token' => $csrfToken,
+            );
+
+  	 return $this->render('app/login.html.twig',$data); 
   }
 
   /**
-   * @Route("/app/dashboard", name="app_dashboard")
-   */
+   * @Route("/dashboard", name="app_dashboard")
+  */
 
   public function dashboard(Request $request)
   {
